0f62cbfd2dece7f3ff491a2a3f4b1f63ce49fc8d
Fabian Ostermann <fabian.ostermann@udo.edu>
2023-07-06 18:17:13 +0200

    Started to build wander simulation in scummLoop

--- engines/scumm/scumm.cpp
+++ engines/scumm/scumm.cpp
@@ -2544,6 +2544,15 @@ void ScummEngine_v0::scummLoop(int delta) {
 }
 
 void ScummEngine::scummLoop(int delta) {
+	debug("%s%d", "Enter ScummLoop, delta=", delta);
+
+	 // TODO: make following delayed and random
+	 //       Problem: seems not to change iMuse signals here
+	startScene(15, nullptr, 0);
+	// TODO: for delay, use sum deltas or:
+	//uint32 millis = g_system->getMillis(true);
+
+
 	// Notify the script about how much time has passed, in jiffies
 	if (VAR_TIMER != 0xFF)
 		VAR(VAR_TIMER) = delta;
